3600a18653743b4dd39519b75a1d355f6d08ad5f
David Harris <davlsu14@gmail.com>
2021-04-16 10:10:22 -0500

    Added user input

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What Fahrenheit temperature would you like to ocnvert?");
+
+            var fTemp = Convert.ToDouble(Console.ReadLine());
+
+            var celcius = TempConversion.FahrenheitToCelcius(fTemp);
+
+            Console.WriteLine($"{celcius} degrees celcius");
+
+            Console.WriteLine("What Celcius temperature would you like to ocnvert?");
+
+            var cTemp = Convert.ToDouble(Console.ReadLine());
+
+            var farhenheit = TempConversion.CelciusToFahrenheit(cTemp);
+
+            Console.WriteLine($"{farhenheit} degrees Farhenheit");
+        }
+    }
+}
---
+++ StaticExercise/TempConversion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace StaticExercise
+{
+    public static class TempConversion
+    {
+        public static double FahrenheitToCelcius(double fahrenheit)
+        {
+            var celcius = (fahrenheit - 32) / 1.8;
+
+            return celcius;
+        }
+
+        public static double CelciusToFahrenheit(double celcius)
+        {
+            var fahrenheit = (celcius * 9) / 5 + 32;
+
+            return fahrenheit;
+        }
+    }
+}
